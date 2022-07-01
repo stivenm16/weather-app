97af155959c63927c32e6876142d0bcfcaa5edc7
Kevin <stiivenmendez16@gmail.com>
2022-07-01 17:37:04 -0500

    Card css

--- src/Components/City/City.jsx
+++ src/Components/City/City.jsx
@@ -22,11 +22,11 @@ class City extends React.Component {
                 </div>
                 <img className="iconoClima" src={"http://openweathermap.org/img/wn/"+this.props.city.img+"@2x.png"} width="80" height="80" alt="" />    
                 <div className="container-bottom-city">
-                    <div>
+                    <div className='wind-box'>
                         <p>Wind: {this.props.city.wind}</p>
                         <p>Clouds: {this.props.city.clouds}</p>
                     </div>
-                    <div>
+                    <div className='lat-box'>
                         <p>Longitud: {this.props.city.longitud}</p>
                         <p>Latitud: {this.props.city.latitud}</p>
                     </div>
